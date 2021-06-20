c0581c52d59f20415447a5308f400a3ce35c092a
Daniel <daniel.macias.gomez@gmail.com>
2021-06-20 22:52:03 +0200

    First commit

---
+++ 0003JS.js
@@ -0,0 +1,9 @@
+// The prime factors of 13195 are 5, 7, 13 and 29.
+
+// What is the largest prime factor of the number 600851475143?
+function primes(num) {
+    let largestPrime = 0;
+    for (let i = num; i > 1; i--) {}
+}
+
+console.log(primes(600851475143));
