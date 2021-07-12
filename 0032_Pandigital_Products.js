0c040eca2bec164dc379dc6e43509d36a0390a26
Daniel <daniel.macias.gomez@gmail.com>
2021-07-12 12:29:11 +0200

    Pandigital products

--- 0032_Pandigital_Products.js
+++ 0032_Pandigital_Products.js
@@ -9,3 +9,44 @@
 
 // HINT: Some products can be obtained in more than one way so be sure to only include it once in
 // your sum.
+
+// examples:
+// 12 x 345 = 6789
+// 1 x 2345 = 6789
+function pandigitalProducts() {
+  let sumPandigitals = 0;
+  let prodSet = new Set();
+
+  for (let mult = 12; mult <= 98; mult++) {
+    for (let multipl = 123; multipl <= 987; multipl++) {
+      for (let prod = 1234; prod <= 9876; prod++) {
+        if (!prod.toString().includes("0") && !mult.toString().includes("0") && !multipl.toString().includes("0")) {
+          let set = new Set([...prod.toString(), ...mult.toString(), ...multipl.toString()]);
+
+          if (set.size === 9 && mult * multipl === prod && !prodSet.has(prod)) {
+            prodSet.add(prod);
+            sumPandigitals += prod;
+          }
+        }
+      }
+    }
+  }
+
+  for (let mult = 1; mult <= 9; mult++) {
+    for (let multipl = 1234; multipl <= 9876; multipl++) {
+      for (let prod = 1234; prod <= 9876; prod++) {
+        if (!prod.toString().includes("0") && !mult.toString().includes("0") && !multipl.toString().includes("0")) {
+          let set = new Set([...prod.toString(), ...mult.toString(), ...multipl.toString()]);
+
+          if (set.size === 9 && mult * multipl === prod && !prodSet.has(prod)) {
+            prodSet.add(prod);
+            sumPandigitals += prod;
+          }
+        }
+      }
+    }
+  }
+  return sumPandigitals;
+}
+
+console.log(pandigitalProducts());
